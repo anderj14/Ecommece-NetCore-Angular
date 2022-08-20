3ba0dd11de8a266ceb8e0199bd0e3e50bf2939c4
anderj14 <andersonfrias001@gmail.com>
2022-08-20 11:04:47 -0400

    Version 9

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { BreadcrumbModule } from 'xng-breadcrumb';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -18,12 +19,13 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
     NotFoundComponent,
     ServerErrorComponent,
     SectionHeaderComponent,
-    
+
   ],
   imports: [
     CommonModule,
     RouterModule,
     BreadcrumbModule,
+    SharedModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
